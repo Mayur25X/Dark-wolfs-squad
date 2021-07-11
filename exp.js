43c5624e5c5ff578c258e7415a088e45d30136b7
mayur uikey <85844462+Mayur25X@users.noreply.github.com>
2021-07-11 16:47:56 +0530

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 4000;
+const home = fs.readFileSync('index.html')
+
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    url = req.url;
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    if (url == '/') {
+        res.end(home);
+
+    }
+    else {
+        res.statusCode = 404;
+        res.end("<h1>404 not found</h1>");
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
